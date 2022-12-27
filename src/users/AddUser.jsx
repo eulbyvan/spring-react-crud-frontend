b01eeb85f785f03303aa9c0cfbb8bbe3fe0e9689
eulbyvan <work.issdq@gmail.com>
2022-12-27 13:58:39 +0700

    06-axios-post

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
 
 /*
  * Author : Stu (https://www.eulbyvan.com)
@@ -7,6 +10,8 @@ import React, { useState } from 'react';
  */
 
 const AddUser = () => {
+	let navigate = useNavigate();
+
 	const [user, setUser] = useState({
 		name: '',
 		username: '',
@@ -19,56 +24,79 @@ const AddUser = () => {
 		setUser({ ...user, [e.target.name]: e.target.value });
 	};
 
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		await axios.post('http://localhost:8080/api/1.0/users', user);
+		navigate('/');
+	};
+
 	return (
 		<div className="container">
 			<div className="row">
 				<div className="col-md-6 offset-md-3 border roudned p-4 mt-2 shadow">
 					<h2 className="text-center m-4">Register User</h2>
-					<div className="mb-3">
-						<label htmlFor="Name" className="form-label">
-							Name
-						</label>
-						<input
-							type="text"
-							className="form-control"
-							placeholder="Enter your name"
-							name="name"
-							value={name}
-							onChange={(e) => onInputChange(e)}
-						/>
-					</div>
-					<div className="mb-3">
-						<label htmlFor="Username" className="form-label">
-							Username
-						</label>
-						<input
-							type="text"
-							className="form-control"
-							placeholder="Enter your username"
-							name="username"
-							value={username}
-							onChange={(e) => onInputChange(e)}
-						/>
-					</div>
-					<div className="mb-3">
-						<label htmlFor="Email" className="form-label">
-							E-mail
-						</label>
-						<input
-							type="text"
-							className="form-control"
-							placeholder="Enter your e-mail address"
-							name="email"
-							value={email}
-							onChange={(e) => onInputChange(e)}
-						/>
-					</div>
-					<button type="submit" className="btn btn-outline-primary">
-						Submit
-					</button>
-					<button className="btn btn-outline-danger mx-2">
-						Cancel
-					</button>
+					<form onSubmit={(e) => onSubmit(e)}>
+						<div className="form-floating mb-3">
+							<input
+								id="floatingInput"
+								type="text"
+								className="form-control"
+								placeholder="Enter your name"
+								name="name"
+								value={name}
+								onChange={(e) => onInputChange(e)}
+							/>
+							<label
+								htmlFor="floatingInput"
+								className="form-label"
+							>
+								Name
+							</label>
+						</div>
+						<div className="form-floating mb-3">
+							<input
+								id="floatingInput"
+								type="text"
+								className="form-control"
+								placeholder="Enter your username"
+								name="username"
+								value={username}
+								onChange={(e) => onInputChange(e)}
+							/>
+							<label
+								htmlFor="floatingInput"
+								className="form-label"
+							>
+								Username
+							</label>
+						</div>
+						<div className="form-floating mb-3">
+							<input
+								id="floatingInput"
+								type="text"
+								className="form-control"
+								placeholder="Enter your e-mail"
+								name="email"
+								value={email}
+								onChange={(e) => onInputChange(e)}
+							/>
+							<label
+								htmlFor="floatingInput"
+								className="form-label"
+							>
+								E-mail
+							</label>
+						</div>
+						<button
+							type="submit"
+							className="btn btn-outline-primary"
+						>
+							Submit
+						</button>
+						<Link className="btn btn-outline-danger mx-2" to="/">
+							Cancel
+						</Link>
+					</form>
 				</div>
 			</div>
 		</div>
