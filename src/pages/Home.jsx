2bfe64d41d1de6f4ad7b82cfd4b1c8895e647322
eulbyvan <work.issdq@gmail.com>
2022-12-27 14:54:56 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 /*
  * Author : Stu (https://www.eulbyvan.com)
@@ -11,6 +11,8 @@ import { Link } from 'react-router-dom';
 const Home = () => {
 	const [users, setUsers] = useState([]);
 
+	const { id } = useParams();
+
 	useEffect(() => {
 		loadUsers();
 	}, []);
@@ -19,6 +21,11 @@ const Home = () => {
 		const result = await axios.get('http://localhost:8080/api/1.0/users');
 		console.log(result.data);
 		setUsers(result.data);
+	};
+
+	const deleteUser = async (id) => {
+		await axios.delete(`http://localhost:8080/api/1.0/users/${id}`);
+		loadUsers();
 	};
 
 	return (
@@ -53,7 +60,10 @@ const Home = () => {
 									>
 										Edit
 									</Link>
-									<button className="btn btn-danger mx-2">
+									<button
+										className="btn btn-danger mx-2"
+										onClick={() => deleteUser(user.id)}
+									>
 										Delete
 									</button>
 								</td>
