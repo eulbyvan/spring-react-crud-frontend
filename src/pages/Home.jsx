8bacb6b0ad59052e1402c5721437aa7f4e6bdb30
eulbyvan <work.issdq@gmail.com>
2022-12-27 11:48:55 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,9 +27,10 @@ const Home = () => {
 					<thead>
 						<tr>
 							<th scope="col">#</th>
-							<th scope="col">First</th>
-							<th scope="col">Last</th>
-							<th scope="col">Handle</th>
+							<th scope="col">Name</th>
+							<th scope="col">Username</th>
+							<th scope="col">Email</th>
+							<th scope="col">Action</th>
 						</tr>
 					</thead>
 					<tbody>
